0aeab6895be6f0f3848154260f16415a8c7200c9
Hoanvuonq <hoanvuonq2002@gmail.com>
2025-12-25 16:36:50 +0700

    update

--- src/app/products/[id]/_components/ProductPurchaseActions/index.tsx
+++ src/app/products/[id]/_components/ProductPurchaseActions/index.tsx
@@ -5,7 +5,7 @@ import { ProductStats, VoucherList } from "@/app/products/_components";
 import Pricing from "@/app/products/_components/Pricing";
 import { ShippingInfo } from "@/app/products/_components/ShippingInfo";
 import { VariantSelector } from "@/app/products/_components/VariantSelector";
-import { AddToCartButton } from "@/components/addToCartButton";
+import { AddToCartButton } from "@/components";
 import { TagComponents } from "@/components/tags";
 import { cn } from "@/utils/cn";
 import { ProductPurchaseActionsProps } from "../../_types/products";
